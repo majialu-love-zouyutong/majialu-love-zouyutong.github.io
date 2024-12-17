11c93208555b0655d7519e44a69d70984e0cbc6f
mjl <2404816298@qq.com>
2024-12-17 19:31:18 +0800

    ES6结束

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-function sum(a,b) {
-  return ()=>{
-    console.log(arguments)
-  }
+class User {
+  constructor() {}
 }
 
-function test(x,y) {
-  sum(3,4)()
+function ConstructorProxy(Class, ...propNames) {
+  return new Proxy(Class, {
+    construct(target, argumentsList) {
+      const obj = Reflect.construct(target, argumentsList);
+      propNames.forEach((name, i) => {
+        obj[name] = argumentsList[i];
+      });
+      return obj;
+    },
+  });
 }
 
-test(1,2)
+const UserProxy = ConstructorProxy(User, 'firstName', 'lastName', 'age');
+
+const user = new UserProxy('张', '三', 18);
+
+console.log(user);
