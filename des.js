1b7a61ee98457991cd4675b547929658c2c6f097
jialuma <jialuma@tencent.com>
2025-07-29 18:57:13 +0800

    feat(手写): promise静态方法

--- des.js
+++ des.js
@@ -1,7 +1,24 @@
-function f(x) {
-  var x = 200;
-  console.log(x);
-  console.log(x);
-  }
-f(a = 100);
+Promise.myRace = (promises) => {
+  return new Promise((resolve, reject) => {
+    // 获取数组长度
+    const len = promises.length;
+
+    // 如果为空数组，则永远保持pending
+    if (len === 0) return;
+
+    // 遍历promises
+    for (let i = 0; i < len; i++) {
+      Promise.resolve(promises[i]).then(resolve, reject);
+    }
+  })
+}
+const a = Promise.race([1, 2, 3]);
+const b = Promise.race([1, 2, 3]);
 console.log(a);
+console.log(b);
+setTimeout(() => {
+  console.log(a);
+}, 1000);
+setTimeout(() => {
+  console.log(b);
+}, 1000);
