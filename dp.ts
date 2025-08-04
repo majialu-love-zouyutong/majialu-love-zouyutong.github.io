28754a10d59b28dac9d5d234b00872205070b93c
jialuma <jialuma@tencent.com>
2025-08-04 23:12:14 +0800

    feat(笔试): 笔试题目

--- dp.ts
+++ dp.ts
@@ -1,25 +1,19 @@
-const zeroOneBagWithOneDimensionalArray = (w: number[], v: number[], totalWeight: number) => {
-  // 剪枝
-  if (w.length === 0 || v.length === 0 || totalWeight === 0) {
-    return 0;
-  }
+const completeBackPack = (weight: number[], value: number[], total: number): number => {
+  // dp[j]表示从0-i任选，容量为j，可重复装，最大价值
+  /**
+   * if (j >= weight[i]) {
+   *    dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]) 
+   *   *}
+   */
 
-  // dp[j]表示背包容量为j时的最大价值
-  const dp = new Array(totalWeight + 1).fill(0);
+  const dp = new Array(total + 1).fill(0);
 
-  // 遍历物品
-  for (let i = 0; i < w.length; i++) {
-    // 遍历背包容量，一定要从后向前遍历，因为dp[j]依赖于dp[j - w[i]]
-    for (let j = totalWeight; j >= w[i]; j--) {
-      dp[j] = Math.max(dp[j], dp[j - w[i]] + v[i]);
+  const len = weight.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = weight[i]; j <= total; j++) {
+      dp[j] = Math.max(dp[j], dp[j - weight[i]] + value[i]);
     }
   }
-
-  return dp[totalWeight];
-};
-
-// 写几组测试用例
-
-console.log(zeroOneBagWithOneDimensionalArray([1, 2, 3], [6, 10, 12], 5));      // 22
-console.log(zeroOneBagWithOneDimensionalArray([2, 3, 4], [3, 4, 5], 5));        // 7
-console.log(zeroOneBagWithOneDimensionalArray([1, 3, 4], [15, 20, 30], 4));     // 35
+  return dp[total];
+}
